31765cb4f8ac4539b3ca2bf09b57de8acdc94982
Thiago Aguiar <thiago.aguiar86@gmail.com>
2023-01-05 19:13:14 -0300

    app me indica aê

--- src/js/main.js
+++ src/js/main.js
@@ -12,3 +12,46 @@ $('.modal').modal(
         show: false
     }
 );
+
+// google form sheet
+function handleSubmit(event) {
+
+    event.preventDefault();
+
+    // dados morador
+    var nomemoradorinput = document.querySelector('input[name=nomemorador]').value;
+    var celularmoradorinput = document.querySelector('input[name=celularmorador]').value;
+    var aptomoradorinput = document.querySelector('input[name=aptomorador]').value;
+    
+    // dados profissional
+    var nomeprofissionalinput = document.querySelector('input[name=nomeprofissional]').value;
+    var categoriaprofissionalinput = document.querySelectorAll('option:checked');
+    var celularprofissionalinput = document.querySelector('input[name=celularprofissional]').value;
+    var emailprofissionalinput = document.querySelector('input[name=emailprofissional]').value;
+    var siteprofissionalinput = document.querySelector('input[name=siteprofissional]').value;
+    var indicandoprofissionalinput = document.querySelector('textarea[name=indicandoprofissional]').value;
+
+    fetch('https://api.sheetmonkey.io/form/eq6HsrSoaM9oFxP9531Lae', {
+        method: 'post',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(
+            {
+                nomemorador: nomemoradorinput,
+                celularmorador: celularmoradorinput,
+                aptomorador: aptomoradorinput,
+
+                nomeprofissional: nomeprofissionalinput,
+                categoriaprofissional: categoriaprofissionalinput,
+                celularprofissional: celularprofissionalinput,
+                emailprofissional: emailprofissionalinput,
+                siteprofissional: siteprofissionalinput,
+                indicandoprofissional: indicandoprofissionalinput,
+            }
+        )
+    });
+}
+
+document.querySelector('form').addEventListener('submit', handleSubmit);
