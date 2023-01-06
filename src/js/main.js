5d021eeddd7ba504b7e7c7c70d6889800f6dc167
Thiago Aguiar <thiago.aguiar86@gmail.com>
2023-01-06 02:00:49 -0300

    app me indica aê

--- src/js/main.js
+++ src/js/main.js
@@ -56,5 +56,101 @@ function handleSubmit(event) {
         window.location.href = ("http://www.w3schools.com")
     );
 }
-
 // document.querySelector('form').addEventListener('submit', handleSubmit);
+
+
+// get json profissionais
+var getProfissionaisJSON = {
+    init: function() {
+        this.listAll();
+        this.engine();
+    },
+    listAll: function() {
+        var el = $('#listaprofissionais');
+
+        $.ajax({
+            type: "GET",
+            url: "https://opensheet.elk.sh/1f-Cq5gpr03s6C0NZGdJH42LLFVp7UVn2YhSBQIdD7Po/Profissionais",
+            success: function(result) {
+                
+                // console.log(result);
+                var output = '';
+
+                for (var i in result) {                            
+                    output += '<div class="col-lg-12 col-md-12 col-sm-12 col-12">';
+                    output += '<h3>'+ result[i].nomeprofissional +'</h3>';
+                    output += '<h4><strong>'+result[i].categoriaprofissional+'</strong></h4>';
+                    output += '<p><i class="fa fa-whatsapp" aria-hidden="true"></i> <a href="#">'+ result[i].celularprofissional +'</a></p>';
+                    output += '<p><i class="fa fa-envelope" aria-hidden="true"></i> <a href="mailto:">'+ result[i].emailprofissional +'</a></p>';
+                    output += '<p><i class="fa fa-globe" aria-hidden="true"></i> <a href='+ result[i].siteprofissional +' target="_blank">'+ result[i].siteprofissional +'</a></p>';
+                    output += '<hr>';
+                    output += '<p class="quemindicou"><strong>Por: </strong><em>'+ result[i].nomemorador +'</em></p>';
+                    output += '<p class="comentario">'+ result[i].indicandoprofissional +'</p>';
+                    output += '</div>';
+                }
+
+                el.html(output);
+            }
+        });
+
+    },
+    engine: function() {
+        
+        var el = $('#listaprofissionais');
+        var filter = $('#categoriaprofissional');
+
+        $.ajax({
+            type: "GET",
+            url: "https://opensheet.elk.sh/1f-Cq5gpr03s6C0NZGdJH42LLFVp7UVn2YhSBQIdD7Po/Profissionais",
+            success: function(result) {
+                
+                // console.log(result);
+                var output;
+
+                filter.on(
+                    'change',
+                    function() {
+                        
+                        var filterSelected = $(this).val();
+                        console.log(filterSelected);
+                        
+                        output = '';
+                        
+                        for (var i in result) {
+                            
+                            if(result[i].categoriaprofissional == filterSelected) {
+                                output += '<div class="col-lg-12 col-md-12 col-sm-12 col-12">';
+                                output += '<h3>'+ result[i].nomeprofissional +'</h3>';
+                                output += '<h4><strong>'+result[i].categoriaprofissional+'</strong></h4>';
+                                output += '<p><i class="fa fa-whatsapp" aria-hidden="true"></i> <a href="#">'+ result[i].celularprofissional +'</a></p>';
+                                output += '<p><i class="fa fa-envelope" aria-hidden="true"></i> <a href="mailto:">'+ result[i].emailprofissional +'</a></p>';
+                                output += '<p><i class="fa fa-globe" aria-hidden="true"></i> <a href='+ result[i].siteprofissional +' target="_blank">'+ result[i].siteprofissional +'</a></p>';
+                                output += '<hr>';
+                                output += '<p class="quemindicou"><strong>Por: </strong><em>'+ result[i].nomemorador +'</em></p>';
+                                output += '<p class="comentario">'+ result[i].indicandoprofissional +'</p>';
+                                output += '</div>';
+                                
+                            } else if(filterSelected == 'todos') {
+
+                                output += '<div class="col-lg-12 col-md-12 col-sm-12 col-12">';
+                                output += '<h3>'+ result[i].nomeprofissional +'</h3>';
+                                output += '<h4><strong>'+result[i].categoriaprofissional+'</strong></h4>';
+                                output += '<p><i class="fa fa-whatsapp" aria-hidden="true"></i> <a href="#">'+ result[i].celularprofissional +'</a></p>';
+                                output += '<p><i class="fa fa-envelope" aria-hidden="true"></i> <a href="mailto:">'+ result[i].emailprofissional +'</a></p>';
+                                output += '<p><i class="fa fa-globe" aria-hidden="true"></i> <a href='+ result[i].siteprofissional +' target="_blank">'+ result[i].siteprofissional +'</a></p>';
+                                output += '<hr>';
+                                output += '<p class="quemindicou"><strong>Por: </strong><em>'+ result[i].nomemorador +'</em></p>';
+                                output += '<p class="comentario">'+ result[i].indicandoprofissional +'</p>';
+                                output += '</div>';
+                            }
+                        }
+        
+                        el.html(output);
+
+                    }
+                )
+            }
+        });
+    }
+}
+getProfissionaisJSON.init();
