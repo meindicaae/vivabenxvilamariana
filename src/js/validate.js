311a13343d80ff9bd61bb4097681e033a1b2cb21
Thiago Aguiar <thiago.aguiar86@gmail.com>
2023-01-12 16:35:24 -0300

    recaptcha

--- src/js/validate.js
+++ src/js/validate.js
@@ -112,3 +112,19 @@ $("input[id*='cpfcnpj']").inputmask({
 //     scaleCaptcha();
 // });
 // scaleCaptcha();
+
+document.getElementById("form").addEventListener("submit",function(evt)
+  {
+  
+  var response = grecaptcha.getResponse();
+  if(response.length == 0) 
+  { 
+    //reCaptcha not verified
+    alert("please verify you are humann!"); 
+    evt.preventDefault();
+    return false;
+  }
+  //captcha verified
+  //do the rest of your validations here
+  
+});
